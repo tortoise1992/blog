0acec3afa160ec78ec31f7f3b704813226ebffba
tortoise1992 <772084015@qq.com>
2020-03-02 10:28:00 +0800

    初次提交

---
+++ app/controller/default/home.js
@@ -0,0 +1,10 @@
+const Controller = require('egg').Controller
+
+class HomeController extends Controller{
+
+    async index(){
+        this.ctx.body="api接口"
+    }
+}
+
+module.exports = HomeController
